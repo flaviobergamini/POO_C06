21b0cf187b08fe54144b5dc0526778c7d327386f
flaviobergamini <flaviohenrique@gec.inatel.br>
2026-04-28 13:02:31 -0300

    feat: adding chapter 9 and 10 examples

---
+++ Capítulo 9 - Classes Abstratas/Exercícios da aula/Exercício 1 - Mamíferos/Mamifero.java
@@ -0,0 +1,18 @@
+package org.example;
+
+public abstract class Mamifero {
+    protected String nome;
+    protected double vida;
+
+    public Mamifero(String nome, double vida) {
+        this.nome = nome;
+        this.vida = vida;
+    }
+
+    public abstract void emitirSom();
+
+    public void mostraInfo() {
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Vida: " + this.vida);
+    }
+}
